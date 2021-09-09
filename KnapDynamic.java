51959c26fd0e9a20ab62a0959dd66b1170e80ad6
xManticor <judsdeleonx@gmail.com>
2021-09-09 16:47:28 +0800

    Update KnapDynamic.java
    Added error check and print spacing

--- KnapDynamic.java
+++ KnapDynamic.java
@@ -11,12 +11,7 @@ public class KnapDynamic {
     public static void main(String args[]) {
         KnapDynamic app = new KnapDynamic();
         getInputs();
-        // int w = 10;
-        // int n = 4;
-        // int[] val = {10, 40, 30, 50};
-        // int[] wt = {5, 4, 6, 3};
 
-        // Populate base cases
         int[][] mat = new int[n + 1][w + 1];
         for (int r = 0; r < w + 1; r++) {
             mat[0][r] = 0;
@@ -25,26 +20,25 @@ public class KnapDynamic {
             mat[c][0] = 0;
         }
         
-        // Main logic
         for (int item = 1; item <= n; item++) {
             for (int capacity = 1; capacity <= w; capacity++) {
-                int maxValWithoutCurr = mat[item - 1][capacity]; // This is guaranteed to exist
-                int maxValWithCurr = 0; // We initialize this value to 0
+                int maxValWithoutCurr = mat[item - 1][capacity]; 
+                int maxValWithCurr = 0; 
                 
-                int weightOfCurr = wt[item - 1]; // We use item -1 to account for the extra row at the top
-                if (capacity >= weightOfCurr) { // We check if the knapsack can fit the current item
-                    maxValWithCurr = val[item - 1]; // If so, maxValWithCurr is at least the value of the current item
+                int weightOfCurr = wt[item - 1]; 
+                if (capacity >= weightOfCurr) {  
+                    maxValWithCurr = val[item - 1]; 
                     
-                    int remainingCapacity = capacity - weightOfCurr; // remainingCapacity must be at least 0
-                    maxValWithCurr += mat[item - 1][remainingCapacity]; // Add the maximum value obtainable with the remaining capacity
+                    int remainingCapacity = capacity - weightOfCurr;
+                    maxValWithCurr += mat[item - 1][remainingCapacity]; 
                 }
                 
-                mat[item][capacity] = Math.max(maxValWithoutCurr, maxValWithCurr); // Pick the larger of the two
+                mat[item][capacity] = Math.max(maxValWithoutCurr, maxValWithCurr); 
             }
         }
         
-        System.out.println(mat[n][w]); // Final answer
-        System.out.println(Arrays.deepToString(mat)); // Visualization of the table
+        System.out.println(mat[n][w]); 
+        System.out.println(Arrays.deepToString(mat).replace("],", "],\n"));
     }
 
     public static void getInputs() {
@@ -62,13 +56,14 @@ public class KnapDynamic {
             dump = sc.nextLine(); 
             System.out.print("Item name: ");
             name[i] = sc.nextLine();
-            System.out.println("name = " + name[i]);
-            System.out.print("Item weight: ");
-            wt[i] = sc.nextInt();
-            System.out.println("w = " + wt[i]);
-            System.out.print("Item value: ");
-            val[i] = sc.nextInt();
-            System.out.println("v = " + val[i]);
+            while(wt[i] <= 0){
+                System.out.print("Item weight: ");
+                wt[i] = sc.nextInt();
+            }
+            while(val[i] <= 0){
+                System.out.print("Item value: ");
+                val[i] = sc.nextInt();
+            }
         }
     }
 }
